196fa4a7fdef61787304dd37899be5b80ad879e4
ihriyad <ihriyad.dev@gmail.com>
2026-05-06 23:35:58 +0600

    feat: integrate better-auth for authentication
    - Added better-auth and its MongoDB adapter for user authentication.
    - Implemented login and registration pages using react-hook-form for form handling.
    - Created API routes for authentication using better-auth.
    - Set up auth client for managing user sessions and interactions with the auth API.
    - Updated package dependencies to include better-auth and related packages.

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
-
+"use client";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
 const LoginPage = () => {
-    return (
-        <div>
-            <h3>Login page</h3>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const { email, password } = data;
+
+    const { data: res, error } = await authClient.signIn.email({
+      email: email,
+      password: password,
+      callbackURL: "/",
+    });
+    console.log(res, error);
+    if (res) {
+      alert("login success");
+    }
+    if (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <div className="container bg-slate-100 rounded-md mx-auto flex items-center justify-center my-10 w-3xl">
+      <div className="p-8 space-y-2">
+        <h1 className="text-center text-xl font-bold">Login to your account</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Enter Your Email"
+              {...register("email", { required: "Email field is required" })}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Enter Your Password"
+              {...register("password", {
+                required: "Password field is required",
+              })}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <button className="btn w-full mt-2 bg-slate-800 text-white">
+              Login
+            </button>
+          </fieldset>
+          <div className="text-center">
+            <p>Do not have an account?</p>
+            <p>
+              <Link href={"/resister"} className="font-bold text-blue-600">
+                Resister
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/app/(auth)/resister/page.jsx
+++ src/app/(auth)/resister/page.jsx
@@ -1,11 +1,101 @@
-import React from 'react';
+"use client";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const ResisterPage = () => {
-    return (
-        <div>
-            <h3>ResisterPage</h3>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const { name, email, password, photo } = data;
+
+    const { data:res, error } = await authClient.signUp.email({
+      name: name,
+      email: email,
+      password: password,
+      photo: photo,
+    });
+    console.log(res, error);
+    if(error){
+      alert(error.message)
+    }
+    if(res){
+      alert("SignUp success")
+    }
+  };
+  return (
+    <div className="container bg-slate-100 rounded-md mx-auto flex items-center justify-center my-10 w-3xl">
+      <div className="p-8 space-y-2 w-2xl">
+        <h1 className="text-center text-xl font-bold">Create Your Account</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Your Name</legend>
+            <input
+              {...register("name", { required: "Name field is required" })}
+              type="text"
+              className="input w-full"
+              placeholder="Enter Your Name"
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Enter Your Email"
+              {...register("email", { required: "Email field is required" })}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Photo URL</legend>
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Enter Your Photo URL"
+              {...register("photo", { required: "photo field is required" })}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Enter Your Password"
+              {...register("password", {
+                required: "Password field is required",
+              })}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <button className="btn w-1/2 mx-auto mt-2 bg-slate-800 text-white">
+              Resister
+            </button>
+          </fieldset>
+          <div className="text-center">
+            <p>Already have an account?</p>
+            <p>
+              <Link href={"/login"} className="font-bold text-blue-600">
+                Login
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ResisterPage;
